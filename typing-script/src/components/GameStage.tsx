947d087d29a07f5086d90e895bc670b17478bf79
kenshin <tanaka.aa@icloud.com>
2021-08-03 17:04:44 -0700

    test keyEvent

--- typing-script/src/components/GameStage.tsx
+++ typing-script/src/components/GameStage.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
+import { useDispatch } from "react-redux";
 import { game, gamedisplay, gameinfo, textblock } from "./style";
 import AlphabetLabel from "./AlphabetLabel";
 import RawStringLabel from "./RawStringLabel";
 import GameInfo from "./GameInfo";
 
 const GameStage: React.FC = () => {
+  const keyLog = useCallback((event) => {
+    console.log(String.fromCharCode(event.keyCode), "is Pressed!!");
+  }, []);
+
+  useEffect(() => {
+    // 最初だけ登録してる
+    window.addEventListener("keydown", keyLog, false);
+    return () => {
+      window.removeEventListener("keydown", keyLog, false);
+    };
+  }, []);
+
   return (
     <div style={game}>
       <div style={gameinfo}>
